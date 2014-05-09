5309c49cb2d9018e513cbf583bad024b05778856
Bomani <abw5248@rit.edu>
2014-05-09 08:47:58 -0400

    Adding the needed scripts for the Gibberish Generation
    Graph, Word, and Markov scripts added
    Still need to add the textToSpeech dll and script

---
+++ 3_10NavMeshAgent/Assets/Scripts/Word.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Word : MonoBehaviour {
+
+	private string wordSt;		// The word as a string
+	private int count = 1;		// The count of this word in corpus
+	private int offset;			// Offset of the word in the graph arrays
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public Word(string st, int off)
+	{
+		wordSt = st;
+		offset = off;
+		count = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public string WordSt
+	{
+		get { return wordSt; }
+		set { wordSt = value; }
+	}
+	
+	public int Offset
+	{
+		get { return offset; }
+		set { offset = value; }
+	}
+	
+	public int Count
+	{
+		get { return count; }
+		set { count = value; }
+	}
+	
+	// override ToString
+	public override string ToString()
+	{
+		return wordSt + " - Count: " + count + " Offset: " + offset;
+	}
+}
